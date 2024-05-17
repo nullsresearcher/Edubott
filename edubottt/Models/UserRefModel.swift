e43412e777efedc3bbdf7f56473a9e16f110803d
mochy99 <mochynguyen99@gmail.com>
2024-05-16 23:36:38 -0700

    added Quiz View

--- edubottt/Models/UserRefModel.swift
+++ edubottt/Models/UserRefModel.swift
@@ -1,19 +1,11 @@
-//
-//  UserRefModel.swift
-//  edubottt
-//
-//  Created by Mochy on 2024-05-13.
-//
-
 import Foundation
 
 struct UserRefModel: Identifiable, Codable {
     var id = UUID()
-    
     var subjectDescription: Subject
     var questions: [Question]
-    
 }
+
 struct Subject: Codable {
     var category: Category
     var dateCreated: Date
@@ -24,7 +16,7 @@ struct Subject: Codable {
         case History = "History"
         case Literature = "Literature"
         case Physics = "Physics"
-        case Chemitry = "Chemistry"
+        case Chemistry = "Chemistry"
         case Biology = "Biology"
     }
     
@@ -35,6 +27,7 @@ struct Subject: Codable {
         _ = Category(rawValue: subject)
     }
 }
+
 struct Question: Codable {
     var question: String
     var options: [String]
@@ -45,64 +38,64 @@ struct Question: Codable {
 
 extension UserRefModel {
     static let test: [UserRefModel] = [
-        UserRefModel(subjectDescription: Subject(category: .Science, 
+        UserRefModel(subjectDescription: Subject(category: .Science,
                                                  dateCreated: Calendar.current.date(byAdding: .day, value: 46, to: Date())!),
                      questions: [
-                        Question(question: "What is the chemical symbol for water?", 
+                        Question(question: "What is the chemical symbol for water?",
                                  options: ["H2O","CO2","NaCl"],
-                                 answers: ["H20"],
-                                 keyWords: ["H20"],
+                                 answers: ["H2O"],
+                                 keyWords: ["H2O"],
                                  isCorrect: false),
-                        Question(question: "Which element has the chemical symbol 'Na'?", 
+                        Question(question: "Which element has the chemical symbol 'Na'?",
                                  options: ["Nitrogen", "Sodium", "Nickel"],
                                  answers: ["Sodium"],
                                  keyWords: ["chemistry", "Na", "element"],
                                  isCorrect: false),
-                        Question(question: "What is the pH value of a neutral solution?", 
+                        Question(question: "What is the pH value of a neutral solution?",
                                  options: ["7", "5", "10"],
                                  answers: ["7"],
-                                 keyWords: ["chemistry", "pH", "neutral"], 
+                                 keyWords: ["chemistry", "pH", "neutral"],
                                  isCorrect: false)
                      ]),
-        UserRefModel(subjectDescription: Subject(category: .Math, 
+        UserRefModel(subjectDescription: Subject(category: .Math,
                                                  dateCreated: Calendar.current.date(byAdding: .day, value: 26, to: Date())!),
                      questions: [
                         Question(
-                            question: "What is the square root of 16?", 
+                            question: "What is the square root of 16?",
                             options: ["3", "4", "5"],
                             answers: ["4"],
-                            keyWords: ["math", "square root"], 
+                            keyWords: ["math", "square root"],
                             isCorrect: false),
-                        Question(question: "Solve for x: 3x - 2 = 10", 
+                        Question(question: "Solve for x: 3x - 2 = 10",
                                  options: ["3", "4", "5"],
                                  answers: ["4"],
                                  keyWords: ["math", "equation"],
                                  isCorrect: false),
-                        Question(question: "What is the value of π (pi) correct to two decimal places?", 
+                        Question(question: "What is the value of π (pi) correct to two decimal places?",
                                  options: ["3.14", "3.16", "3.18"],
                                  answers: ["3.14"],
                                  keyWords: ["math","pi"],
                                  isCorrect: false),
-                        Question(question: "What is the area of a rectangle with length 6 units and width 4 units?", 
+                        Question(question: "What is the area of a rectangle with length 6 units and width 4 units?",
                                  options: ["20", "24", "30"],
                                  answers: ["24"],
                                  keyWords: ["math", "rectangle", "area"],
                                  isCorrect: false)
                      ]),
-        UserRefModel(subjectDescription: Subject(category: .Math, 
+        UserRefModel(subjectDescription: Subject(category: .Math,
                                                  dateCreated: Calendar.current.date(byAdding: .day, value: 15, to: Date())!),
                      questions: [
-                        Question(question: "What is the square root of 25?", 
+                        Question(question: "What is the square root of 25?",
                                  options: ["3", "4", "5"],
                                  answers: ["5"],
                                  keyWords: ["math", "square root"],
                                  isCorrect: false),
-                        Question(question: "Solve for x: 2x + 3 = 11", 
+                        Question(question: "Solve for x: 2x + 3 = 11",
                                  options: ["3", "4", "5"],
                                  answers: ["4"],
                                  keyWords: ["math", "equation"],
                                  isCorrect: false),
-                        Question(question: "What is the value of π (pi) correct to two decimal places?", 
+                        Question(question: "What is the value of π (pi) correct to two decimal places?",
                                  options: ["3.14", "3.16", "3.18"],
                                  answers: ["3.14"],
                                  keyWords: ["math", "pi"],
@@ -114,10 +107,10 @@ extension UserRefModel {
                             keyWords: ["math", "triangle", "area"],
                             isCorrect: false)
                      ]),
-        UserRefModel(subjectDescription: Subject(category: .Physics, 
+        UserRefModel(subjectDescription: Subject(category: .Physics,
                                                  dateCreated: Calendar.current.date(byAdding: .day, value: 6, to: Date())!),
                      questions: [
-                        Question(question: "What is Newton's first law of motion?", 
+                        Question(question: "What is Newton's first law of motion?",
                                  options: [
                                     "An object in motion tends to stay in motion, and an object at rest tends to stay at rest unless acted upon by an external force.",
                                     "The force exerted by an object is equal to the mass of the object multiplied by its acceleration.",
@@ -125,26 +118,26 @@ extension UserRefModel {
                                  answers: ["An object in motion tends to stay in motion, and an object at rest tends to stay at rest unless acted upon by an external force."],
                                  keyWords: ["physics", "Newton", "motion", "law"],
                                  isCorrect: false),
-                        Question(question: "What is the SI unit of force?", 
+                        Question(question: "What is the SI unit of force?",
                                  options: ["Watt", "Joule", "Newton"],
-                                 answers: ["Newton"], 
+                                 answers: ["Newton"],
                                  keyWords: ["physics", "force", "unit", "SI"],
                                  isCorrect: false),
-                        Question(question: "What is the acceleration due to gravity on Earth's surface?", 
-                                 options: ["9.8 m/s²", "6.67 x 10^-11 Nm²/kg²", "3 x 10^8 m/s"], 
+                        Question(question: "What is the acceleration due to gravity on Earth's surface?",
+                                 options: ["9.8 m/s²", "6.67 x 10^-11 Nm²/kg²", "3 x 10^8 m/s"],
                                  answers: ["9.8 m/s²"],
-                                 keyWords: ["physics", "gravity", "acceleration", "Earth"], 
+                                 keyWords: ["physics", "gravity", "acceleration", "Earth"],
                                  isCorrect: false),
-                        Question(question: "What is the formula to calculate kinetic energy?", 
-                                 options: ["KE = 1/2 * m * v^2", "KE = m * g * h", "KE = F * d"], 
+                        Question(question: "What is the formula to calculate kinetic energy?",
+                                 options: ["KE = 1/2 * m * v^2", "KE = m * g * h", "KE = F * d"],
                                  answers: ["KE = 1/2 * m * v^2"],
-                                 keyWords: ["physics", "kinetic energy", "formula", "velocity", "mass"], 
+                                 keyWords: ["physics", "kinetic energy", "formula", "velocity", "mass"],
                                  isCorrect: false)
                      ]),
-        UserRefModel(subjectDescription: Subject(category: .Biology, 
+        UserRefModel(subjectDescription: Subject(category: .Biology,
                                                  dateCreated: Calendar.current.date(byAdding: .day, value: 6, to: Date())!),
                      questions: [
-                        Question(question: "What is the powerhouse of the cell?", 
+                        Question(question: "What is the powerhouse of the cell?",
                                  options: ["Ribosome", "Nucleus", "Mitochondria"],
                                  answers: ["Mitochondria"],
                                  keyWords: ["biology", "cell", "organelle", "mitochondria"],
@@ -154,17 +147,16 @@ extension UserRefModel {
                                  answers: ["Photosynthesis"],
                                  keyWords: ["biology", "plants", "food", "photosynthesis"],
                                  isCorrect: false),
-                        Question(question: "What is the smallest unit of life?", 
+                        Question(question: "What is the smallest unit of life?",
                                  options: ["Cell", "Atom", "Molecule"],
                                  answers: ["Cell"],
                                  keyWords: ["biology", "life", "unit", "cell"],
                                  isCorrect: false),
-                        Question(question: "Which scientist is known as the father of modern genetics?", 
+                        Question(question: "Which scientist is known as the father of modern genetics?",
                                  options: ["Gregor Mendel", "Charles Darwin", "Louis Pasteur"],
                                  answers: ["Gregor Mendel"],
                                  keyWords: ["biology", "genetics", "scientist", "Mendel"],
                                  isCorrect: false)
                      ])
     ]
-
 }
---
+++ edubottt/Views/Main/Quiz/QuizView.swift
@@ -0,0 +1,65 @@
+//
+//  QuizView.swift
+//  edubottt
+//
+//  Created by Mochy on 2024-05-16.
+//
+
+import SwiftUI
+
+
+// Main QuizView
+struct QuizView: View {
+    let subject: UserRefModel
+    @State private var currentIndex: Int = 0
+    
+    var body: some View {
+        let columns = [GridItem(), GridItem()]
+        
+        VStack {
+            Text(subject.questions[currentIndex].question)
+                .font(.headline)
+                .padding()
+            
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(subject.questions[currentIndex].options, id: \.self) { option in
+                    OptionView(option: option)
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("Quiz")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+// OptionView
+struct OptionView: View {
+    let option: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .frame(width: 180, height: 100)
+                .cornerRadius(10)
+                .foregroundColor(.gray.opacity(0.7))
+                .shadow(color: Color.black.opacity(0.5), radius: 10, x: 0, y: 10)
+            
+            Text(option)
+                .frame(width: 160, height: 100, alignment: .center)
+                .font(.title2)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct Quiz_Previews: PreviewProvider {
+    static var previews: some View {
+        let userRefViewModel = UserRefViewModel()
+        let sampleUserRef: UserRefModel = userRefViewModel.filteredUserRef(category: "Math")[0]
+        return QuizView(subject: sampleUserRef)
+    }
+}
+
